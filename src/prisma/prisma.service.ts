f19af71c79e98a5688f36c14e11fbe8be1af1d23
hyeonjae <guswo1013@naver.com>
2023-09-17 03:41:06 +0900

    Setting: 프로젝트 초기 세팅

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,7 @@
+// src/prisma/prisma.service.ts
+
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {}
