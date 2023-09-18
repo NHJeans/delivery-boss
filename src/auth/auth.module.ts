1571001f16fc83d2eadfdf77359d2c8dcd66f4b4
hyeonjae <guswo1013@naver.com>
2023-09-19 01:11:57 +0900

    Setting: feat 브랜치 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 //src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { CustomerJwtStrategy } from './customer.jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 import { CustomerModule } from 'src/customer/customer.module';
-import { OwnerModule } from 'src/owner/owner.module';
 import { CustomerLoginService } from 'src/customer/service/customer.login.service';
+import { OwnerModule } from 'src/owner/owner.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthController } from './auth.controller';
+import { CustomerJwtStrategy } from './customer.jwt.strategy';
 
 export const jwtSecret = 'zjP9h6ZI5LoSKCRj';
 
--- src/auth/customer.jwt.strategy.ts
+++ src/auth/customer.jwt.strategy.ts
@@ -2,9 +2,10 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtSecret } from './auth.module';
 import { CustomerLoginService } from '../customer/service/customer.login.service';
+import { jwtSecret } from './auth.module';
 
+//* JWT 토큰을 이용한 전략 구현
 @Injectable()
 export class CustomerJwtStrategy extends PassportStrategy(Strategy, 'customer-jwt') {
   constructor(private readonly customerLoginService: CustomerLoginService) {
