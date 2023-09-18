33d28c05fc996d50459e21bf9d14e10c8dcb6ec9
SHIN1501020 <shin8hh@gmail.com>
2023-09-18 21:54:53 +0900

    Feat: 추가 class-transformer package

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const config = new DocumentBuilder().setTitle('Median').setDescription('The Median API description').setVersion('0.1').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
